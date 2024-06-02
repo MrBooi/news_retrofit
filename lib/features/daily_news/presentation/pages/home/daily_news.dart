4dc4544efd54729d42645ab4f147f96a48cf1aa3
Ayabonga <ayabongabooi2@gmail.com>
2024-06-02 19:26:08 +0200

    wip: daily news pages added

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_retrofit/features/daily_news/domain/entities/article_entity.dart';
+import 'package:news_retrofit/features/daily_news/presentation/widgets/article_title.dart';
+
+import '../../bloc/article/remote/remote.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(context),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppbar(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        'Daily News',
+        style: TextStyle(color: Colors.black),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () => _onShowSavedArticlesViewTapped(context),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 14),
+            child: Icon(Icons.bookmark, color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticlesState>(
+      builder: (_, state) {
+        if (state is RemoteArticlesLoading) {
+          return const Center(child: CupertinoActivityIndicator());
+        }
+        if (state is RemoteArticlesError) {
+          return const Center(child: Icon(Icons.refresh));
+        }
+        if (state is RemoteArticlesDone) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return ArticleWidget(
+                article: state.articles![index],
+                onArticlePressed: (article) =>
+                    _onArticlePressed(context, article),
+              );
+            },
+            itemCount: state.articles!.length,
+          );
+        }
+        return const SizedBox();
+      },
+    );
+  }
+
+  void _onArticlePressed(BuildContext context, ArticleEntity article) {
+    // TODO go router navigation to ArticleDetails
+  }
+
+  void _onShowSavedArticlesViewTapped(BuildContext context) {
+    // TODO go router navigation to SavedArticles
+  }
+}
