99e54c857cf70377d838e95921985d3c80fb65cc
Ayabonga <ayabongabooi2@gmail.com>
2024-06-02 10:35:14 +0200

    chore: add theme setup

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData get theme {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: 'Muli',
+      appBarTheme: const AppBarTheme(
+        color: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Color(0XFF8B8B8B)),
+        titleTextStyle: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
+      ),
+    );
+  }
+}
